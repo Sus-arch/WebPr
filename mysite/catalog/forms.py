5a81100ffca6d198f60801b2c069ffd8db44492e
Андрей <andrej5122785@yandex.ru>
2025-09-09 23:03:38 +0300

    add validation of negative price by products

--- mysite/catalog/forms.py
+++ mysite/catalog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Product
 
 
@@ -9,5 +10,11 @@ class ProductForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'min': '0', 'step': '0.01'}),
         }
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price < 0:
+            raise ValidationError("Цена не может быть отрицательной.")
+        return price
